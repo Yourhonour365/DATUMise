8740ebfb75b91be105952fb2411b349b59dd5386
Jed Chamberlain <jed@commerxia.io>
2026-03-03 14:27:31 +0000

    Add Comment model

--- datumise-api/posts/models.py
+++ datumise-api/posts/models.py
@@ -19,3 +19,25 @@ class Observation(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Comment(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="comments",
+    )
+    observation = models.ForeignKey(
+        Observation,
+        on_delete=models.CASCADE,
+        related_name="comments",
+    )
+    content = models.TextField(max_length=2000)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"Comment by {self.owner} on {self.observation_id}"
+    
+    
