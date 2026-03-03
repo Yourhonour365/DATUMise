ce94bda245d0f044e6cfc8d8bb6c6172b020f610
Jed Chamberlain <jed@commerxia.io>
2026-03-03 13:46:12 +0000

    Add Observation list and detail API views

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import health
+from .views import ObservationList, ObservationDetail
 
 urlpatterns = [
-    path("health/", health),
+    path("observations/", ObservationList.as_view(), name="observation-list"),
+    path("observations/<int:pk>/", ObservationDetail.as_view(), name="observation-detail"),
 ]
