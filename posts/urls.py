6b8596f22095d827961a05f23cd9abd546eb6289
Jed Chamberlain <jed@commerxia.io>
2026-03-03 14:54:24 +0000

    Add Comment detail endpoint and fix serializer

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ObservationList, ObservationDetail
+from .views import ObservationList, ObservationDetail, CommentList, CommentDetail
 
 urlpatterns = [
     path("observations/", ObservationList.as_view(), name="observation-list"),
     path("observations/<int:pk>/", ObservationDetail.as_view(), name="observation-detail"),
+    path("comments/", CommentList.as_view(), name="comment-list"),
+    path("comments/<int:pk>/", CommentDetail.as_view(), name="comment-detail"),
 ]
