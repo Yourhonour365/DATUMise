ba1f81d6a7b3cea6b836c5a650b3f168b7a70b2b
Jed Chamberlain <jed@commerxia.io>
2026-03-06 10:56:04 +0000

    Add observation edit page and route

---
+++ datumise-react/src/ObservationEditForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+function ObservationEditForm() {
+  return (
+    <Container className="mt-4">
+      <h1>Edit Observation</h1>
+      <p>Edit form will go here.</p>
+    </Container>
+  );
+}
+
+export default ObservationEditForm;
