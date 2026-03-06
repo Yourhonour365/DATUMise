2a7c4df9e602a24bb2e88573c6a0eecd299e51a4
Jed Chamberlain <jed@commerxia.io>
2026-03-06 09:55:16 +0000

    Add observation create page and route

--- datumise-react/src/App.js
+++ datumise-react/src/App.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import api from "./api/api";
-
+import ObservationCreateForm from "./ObservationCreateForm";
+  
 function Home() {
   return (
     <div className="container mt-5">
@@ -65,6 +66,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/observations" element={<Observations />} />
+        <Route path="/observations/create" element={<ObservationCreateForm />} />
       </Routes>
     </Router>
   );
---
+++ datumise-react/src/ObservationCreateForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ObservationCreateForm() {
+  return (
+    <div>
+      <h1>Create Observation</h1>
+      <p>Form coming soon...</p>
+    </div>
+  );
+}
+
+export default ObservationCreateForm;
