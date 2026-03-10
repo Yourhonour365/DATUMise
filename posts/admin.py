8ef3e25a2fbb261d95e651965100b3a4cb07d3aa
Jed Chamberlain <jed@commerxia.io>
2026-03-10 16:23:57 +0000

    Auto-create active survey and attach observations

--- posts/admin.py
+++ posts/admin.py
@@ -5,7 +5,10 @@ from .models import Survey, Observation, Comment
 @admin.register(Survey)
 class SurveyAdmin(admin.ModelAdmin):
     list_display = ("name", "client", "site", "status", "assigned_to", "is_incomplete")
+
+@admin.register(Observation)
+class ObservationAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "owner", "survey", "created_at")
     
-admin.site.register(Observation)
 admin.site.register(Comment)
 
