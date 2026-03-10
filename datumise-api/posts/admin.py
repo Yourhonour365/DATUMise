e5f0862eca20762f7e21332f9344f1e16da49bc3
Jed Chamberlain <jed@commerxia.io>
2026-03-10 16:03:10 +0000

    Add Survey model with lifecycle statuses and incomplete survey indicator

--- datumise-api/posts/admin.py
+++ datumise-api/posts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib import admin
+from .models import Survey, Observation, Comment
 
-# Register your models here.
+@admin.register(Survey)
+class SurveyAdmin(admin.ModelAdmin):
+    list_display = ("name", "client", "site", "status", "assigned_to", "is_incomplete")
+    
+admin.site.register(Observation)
+admin.site.register(Comment)
+
